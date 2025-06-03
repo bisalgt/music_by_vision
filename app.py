286886409500cf8c524b22fe8234c0c060ed83e8
Bishal Gautam <bisalgt@gmail.com>
2025-06-03 15:54:49 +0200

    Adding image and layoutto the GUI

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 import sys
 
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QPushButton, QGridLayout
 
 
-# Subclass QMainWindow to customize your application's main window
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("My App")
 
-        button = QPushButton("Press Me!")
+        self.label_stream = QLabel(self)
+        self.pixmap = QPixmap("./output/concept_gui_app.png")
+        self.label_stream.setPixmap(self.pixmap)
+        
 
-        self.setFixedSize(QSize(400, 300))
+        self.btn = QPushButton("Button")
+    
+        layout = QGridLayout()
+        layout.addWidget(self.label_stream, 0,0)
+        layout.addWidget(self.btn, 1,0)
+
+        container = QWidget()
+        container.setLayout(layout)
 
         # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        self.setCentralWidget(container)
 
 
 app = QApplication(sys.argv)
